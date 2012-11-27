0e526cb6ee8667a38819604dab3f4fddab91f80b
Dave Levine <dmlevi@gmail.com>
2012-11-27 12:03:55 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+// Model to access contacts
+
+var adressBook = {
+
+	"designers" : [
+		{
+			"name": "Kristy Stetson"
+		},
+		{
+			"name": "Alex Sylvia"	
+		},
+		{
+			"name": "Alec Harrison"	
+		},
+		{
+			"name": "Steve Hickey"
+		},
+		{
+			"name": "Mark Grambau"
+		},
+		{
+			"name": "Neal Corbett"
+		},
+		{
+			"name": "Jonathan Barker"
+		}
+	],
+
+	"developers" : [
+	{
+		"name": "Tim Wright"
+	},
+	{
+		"name": "Tim Wright"
+	},
+	{
+		"name": "Sarah Canieso"
+	}
+	{
+		"name": "Jinyoung Chang"
+	},
+	{
+		"name": "Dave Romero"
+	},
+	{
+		"name": "Paul Greenlea"
+	}
+	],
+
+	"strategist" : [
+	{
+		"name": "Steve Benoit"
+	},
+	{
+		"name": "Michael Connors"
+	},
+	{
+		"name": "Geordie Kaytes"
+	}
+	],
+
+	"managers" : [
+	{
+		"name": "Richard Banfield"
+	},
+	{
+		"name": "Alex Fedorov"
+	},
+	{
+		"name": "Dan Allard"
+	},
+	{
+		"name": "Kelly Powell"
+	},
+	{
+		"name": "Mike Banfield"
+	},
+	{
+		"name": "Tim Lupo"
+	},
+	{
+		"name": "Michele Hartog"
+	},
+	{
+		"name": "Michael Perrone"
+	},
+	{
+		"name": "Alex Stetson"
+	},
+	{
+		"name": "Gary Ambrosino"
+	},
+	{
+		"name": "Jason Henrichs"
+	},
+	{
+		"name": "Kitt Williams"
+	}
+	]
+};
