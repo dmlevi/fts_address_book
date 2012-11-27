b2ff5ccc21e9fc0abc6d06e9fbe57e6ddb1c64a0
Dave Levine <dmlevi@gmail.com>
2012-11-27 15:55:50 -0500

    added emails and started methods

--- js/main.js
+++ js/main.js
@@ -3,108 +3,160 @@
 
 	// address book data
 
-var adressBook = {
+var contacts = {
 
-	"designers" : [
+	"addressBook" : [
 		{
 			"name": "Kristy Stetson"
+			"email": "kristy@freshtilledsoil.com"
 		},
 		{
 			"name": "Alex Sylvia"	
+			"email": "alex@freshtilledsoil.com"
 		},
 		{
 			"name": "Alec Harrison"	
+			"email": "alec@freshtilledsoil.com"
 		},
 		{
 			"name": "Steve Hickey"
+			"email": "steve@freshtilledsoil.com"
 		},
 		{
 			"name": "Mark Grambau"
+			"email": "mark@freshtilledsoil.com"
 		},
 		{
 			"name": "Neal Corbett"
+			"email": "neal@freshtilledsoil.com"
 		},
 		{
 			"name": "Jonathan Barker"
+			"email": "jonathan@freshtilledsoil.com"
 		}
-	],
-
-	"developers" : [
-	{
-		"name": "Tim Wright"
-	},
-	{
-		"name": "Tim Wright"
-	},
-	{
-		"name": "Sarah Canieso"
-	}
-	{
-		"name": "Jinyoung Chang"
-	},
-	{
-		"name": "Dave Romero"
-	},
-	{
-		"name": "Paul Greenlea"
-	}
-	],
-
-	"strategist" : [
-	{
-		"name": "Steve Benoit"
-	},
-	{
-		"name": "Michael Connors"
-	},
-	{
-		"name": "Geordie Kaytes"
-	}
-	],
-
-	"managers" : [
-	{
-		"name": "Richard Banfield"
-	},
-	{
-		"name": "Alex Fedorov"
-	},
-	{
-		"name": "Dan Allard"
-	},
-	{
-		"name": "Kelly Powell"
-	},
-	{
-		"name": "Mike Banfield"
-	},
-	{
-		"name": "Tim Lupo"
-	},
-	{
-		"name": "Michele Hartog"
-	},
-	{
-		"name": "Michael Perrone"
-	},
-	{
-		"name": "Alex Stetson"
-	},
-	{
-		"name": "Gary Ambrosino"
-	},
-	{
-		"name": "Jason Henrichs"
-	},
-	{
-		"name": "Kitt Williams"
-	}
+		{
+			"name": "Tim Wright"
+			"email": "tim@freshtilledsoil.com"
+		},
+		{
+			"name": "Sarah Canieso"
+			"email": "sarah@freshtilledsoil.com"
+		}
+		{
+			"name": "Jinyoung Chang"
+			"email": "jinyoung@freshtilledsoil.com"
+		},
+		{
+			"name": "Dave Romero"
+			"email": "dave@freshtilledsoil.com"
+		},
+		{
+			"name": "Paul Greenlea"
+			"email": "paul@freshtilledsoil.com"
+		}
+		{
+			"name": "Steve Benoit"
+			"email": "steve@freshtilledsoil.com"
+		},
+		{
+			"name": "Michael Connors"
+			"email": "michael@freshtilledsoil.com"
+		},
+		{
+			"name": "Geordie Kaytes"
+			"email": "geordie@freshtilledsoil.com"
+		}
+		{
+			"name": "Richard Banfield"
+			"email": "richard@freshtilledsoil.com"
+		},
+		{
+			"name": "Alex Fedorov"
+			"email": "alex@freshtilledsoil.com"
+		},
+		{
+			"name": "Dan Allard"
+			"email": "dan@freshtilledsoil.com"
+		},
+		{
+			"name": "Kelly Powell"
+			"email": "kelly@freshtilledsoil.com"
+		},
+		{
+			"name": "Mike Banfield"
+			"email": "mike@freshtilledsoil.com"
+		},
+		{
+			"name": "Tim Lupo"
+			"email": "tim@freshtilledsoil.com"
+		},
+		{
+			"name": "Michele Hartog"
+			"email": "michele@freshtilledsoil.com"
+		},
+		{
+			"name": "Michael Perrone"
+			"email": "michael@freshtilledsoil.com"
+		},
+		{
+			"name": "Alex Stetson"
+			"email": "alex@freshtilledsoil.com"
+		},
+		{
+			"name": "Gary Ambrosino"
+			"email": "gary@freshtilledsoil.com"
+		},
+		{
+			"name": "Jason Henrichs"
+			"email": "jason@freshtilledsoil.com"
+		},
+		{
+			"name": "Kitt Williams"
+			"email": "kitt@freshtilledsoil.com"
+		}
 	]
 };
 
-// define the DOM elements
+	// define the DOM elements
 
 var searchForm = document.getElementByID("search-form"),
 	searchField = document.getElementByID("search-box"),
+	getAllButton = document.getElementByID("get-all"),
 	count = addressBook.length,
 	target = document.getElementByID("output");
+
+	// define address book methods
+
+var addr = {
+
+	search : function(event) {
+		var searchValue = searchField.value,
+		i;
+
+		event.preventDefault();
+
+		target.innerHTML = "";
+
+		if(count > 0 && searchValue !== "") {
+			for(i = 0; i < count; i = i + 1) {
+
+				var obj = contacts.addressBook[i],
+				isItFound = obj.name.indexOf(searchValue);
+
+				if(isItFound !== -1) {
+
+					target.innerHTML += '<p>' + obj.name + ', <a href="mailto:' + obj.email + '">' + obj.email + '</a><p>';
+			
+				}
+			}
+		}
+	}
+
+	getAllContacts : function () {
+		var i;
+		target.innerHTML = "";
+	}
+}
+
+)}();
+
