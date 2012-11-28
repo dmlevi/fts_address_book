f387f128ef03404083e3985bff1253a703352a3e
Dave Levine <dmlevi@gmail.com>
2012-11-28 14:23:47 -0500

    added event listeners

--- js/main.js
+++ js/main.js
@@ -155,8 +155,39 @@ var addr = {
 	getAllContacts : function () {
 		var i;
 		target.innerHTML = "";
-	}
-}
 
-)}();
+		if(count > 0) {
+			for(i=0; i < count; i = i + 1) {
+
+				var obj = contacts.addressBook[i];
+
+				target.innerHTML += '<p>' + obj.name + ', <a href=mailto:' + obj.email + '">' + obj.email + '</a><p>';
+			}
+		}
+	},
+
+	setActiveSection : function(){
+		this.parentNode.removeAttribute("class");
+	},
+	addHoverClass : function(){
+		searchForm.setAttribute("class", "hovering");
+	},
+	removeHoverClass : function() {
+		searchForm.removeAttribute("class");
+	}
+} 
+
+// end addr object
+
+// activate the event listeners
+
+searchField.addEventListener("keyup", addr.search, false);
+searchField.addEventListener("focus", addr.setActiveSection, false);
+searchField.addEventListener("blur", addr.removeActiveSection, false);
+getAllButton.addEventListener("click", addr.getAllContacts, false);
+searchForm.addEventListener("mouseover", addr.addHoverClass, false);
+searchForm.addEventListener("mouseout", addr.removeHoverClass, false);
+searchForm.addEventListener("submit", addr.search, false);
+
+})();
 
