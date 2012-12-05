7805448278a2d82b7cdd4d3f32b435d99ccca535
Dave Levine <dmlevi@gmail.com>
2012-12-05 11:36:33 -0500

    working on ajax

--- js/main.js
+++ js/main.js
@@ -1,95 +1,26 @@
 // start annoynymous function
 (function() {
 
-    // address book data
-    var contacts = {
 
-        "addressBook": [
-            {
-            "name": "Kristy Stetson",
-            "email": "kristy@freshtilledsoil.com"},
-        {
-            "name": "Alex Sylvia",
-            "email": "alex@freshtilledsoil.com"},
-        {
-            "name": "Alec Harrison",
-            "email": "alec@freshtilledsoil.com"},
-        {
-            "name": "Steve Hickey",
-            "email": "steve@freshtilledsoil.com"},
-        {
-            "name": "Mark Grambau",
-            "email": "mark@freshtilledsoil.com"},
-        {
-            "name": "Neal Corbett",
-            "email": "neal@freshtilledsoil.com"},
-        {
-            "name": "Jonathan Barker",
-            "email": "jonathan@freshtilledsoil.com"},
-        {
-            "name": "Tim Wright",
-            "email": "tim@freshtilledsoil.com"},
-        {
-            "name": "Sarah Canieso",
-            "email": "sarah@freshtilledsoil.com"},
-        {
-            "name": "Jinyoung Chang",
-            "email": "jinyoung@freshtilledsoil.com"},
-        {
-            "name": "Dave Romero",
-            "email": "dave@freshtilledsoil.com"},
-        {
-            "name": "Paul Greenlea",
-            "email": "paul@freshtilledsoil.com"},
-        {
-            "name": "Steve Benoit",
-            "email": "steve@freshtilledsoil.com"},
-        {
-            "name": "Michael Connors",
-            "email": "michael@freshtilledsoil.com"},
-        {
-            "name": "Geordie Kaytes",
-            "email": "geordie@freshtilledsoil.com"},
-        {
-            "name": "Richard Banfield",
-            "email": "richard@freshtilledsoil.com"},
-        {
-            "name": "Alex Fedorov",
-            "email": "alex@freshtilledsoil.com"},
-        {
-            "name": "Dan Allard",
-            "email": "dan@freshtilledsoil.com"},
-        {
-            "name": "Kelly Powell",
-            "email": "kelly@freshtilledsoil.com"},
-        {
-            "name": "Mike Banfield",
-            "email": "mike@freshtilledsoil.com"},
-        {
-            "name": "Tim Lupo",
-            "email": "tim@freshtilledsoil.com"},
-        {
-            "name": "Michele Hartog",
-            "email": "michele@freshtilledsoil.com"},
-        {
-            "name": "Michael Perrone",
-            "email": "michael@freshtilledsoil.com"},
-        {
-            "name": "Alex Stetson",
-            "email": "alex@freshtilledsoil.com"},
-        {
-            "name": "Gary Ambrosino",
-            "email": "gary@freshtilledsoil.com"},
-        {
-            "name": "Jason Henrichs",
-            "email": "jason@freshtilledsoil.com"},
-        {
-            "name": "Kitt Williams",
-            "email": "kitt@freshtilledsoil.com"}
-        ]
-    };
+    function ajaxCall(dataUrl) {
+        
+        var request = getHTTPObject();
+
+        request.onreadystatechange = function() {
+
+            if (request.readyState === 4 && request.status === 200 ) {
+
+                console.log(request.responseText);
+            }
+        },
+
+        request.open("GET", dataUrl, true);
+
+        request.send(null);
+    }
 
     // define the DOM elements
+
     var searchForm = document.getElementById("search-form"),
         searchField = document.getElementById("q"),
         target = document.getElementById("output"),
