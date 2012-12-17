cbd048f9b3e884625ef62041518ee9460f2089be
Dave Levine <dmlevi@gmail.com>
2012-12-17 13:49:13 -0500

    start over from scratch with jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// start annoynymous function
 $(document).ready(function() {
 
 
@@ -9,81 +8,12 @@ $(document).ready(function() {
 
         $('#output').empty();
 
-    });
+        if (count > 0 ) {
 
+            $.each(addrBook, function ( i, obj) {
 
-
-    // define the DOM elements
-    var searchForm = $("#search-form"),
-        searchField = $("#q"),
-        target = $("#output");
-
-
-    var addr = {
-
-        search: function(event) {
-
-            var output = $("#output");
-
-            $.ajax({
-                type: 'GET',
-                url: 'data/contacts.json',
-                dataType: 'json',
-                success: function(data) {
-
-                    var addrBook = data.addressBook,
-                        count = addrBook.length;
-
-                    $('#output').empty();
-
-                    if (count > 0) {
-
-                        $.each(addrBook, function(i) {
-
-                            var obj = addrBook[i],
-
-                                isItFound = obj.name.match(new RegExp(searchValue, "i"));
-
-                            if (isItFound) {
-
-                                $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
-
-                            }
-                        });
-                    }
-
-                }
+                 $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>').hide().fadeIn();
             });
-
-
-            getAllContacts: function() {
-
-                var output = $("#output");
-
-                $.ajax({
-                    type: 'GET',
-                    url: 'data/contacts.json',
-                    dataType: 'json',
-                    success: function(data) {
-
-                        var addrBook = data.addressBook,
-                            count = addrBook.length;
-
-                        $('#output').empty();
-
-                        if (count > 0) {
-
-                            $.each(addrBook, function(i) {
-
-                                var obj = addrBook[i],
-
-                                    $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
-                            }
-                            }
-                        });
-                }
-                };
-
-                searchField.addEventListener("keyup", addr.search, false);
-
-            })();
+        }
+    });
+});
