5265de5e83f785aff0c3169ebe764d199a3c3f1b
Geordie Kaytes <geordie.kaytes@freshtilledsoil.com>
2012-12-19 10:21:15 -0500

    added hints and comments

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
     $.getJSON('data/contacts.json', function(data) {
-
+        // HINT: all of these variables that are getting set here 
+        // are thrown away when the code hits line 10. Research "closures" for why.
         var addrBook = data.addressBook,
             count = addrBook.length,
             searchField = $('#q'),
@@ -9,7 +10,7 @@ $(document).ready(function() {
 
     }); // end ajax call
 
-
+    // HINT: who is receiving the keyup event?
     $('#output').keyup(function(event) {
         
         $('#output').empty();
@@ -18,6 +19,7 @@ $(document).ready(function() {
 
             $.each(addrBook, function(i, obj) {
 
+                // COMMENT: Nice use of regular expressions & case-insensitive searching!
                 isItFound = obj.name.match(new RegExp(searchValue, "i"));
 
                 if (isItFound) {
