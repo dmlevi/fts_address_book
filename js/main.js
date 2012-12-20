71e455ebdbf9999c509643b8422f04807b507441
Dave Levine <dmlevi@gmail.com>
2012-12-20 13:44:51 -0500

    added console.log for troubleshooting

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
 // document.ready waits for the entire dom to load. As soon as the dom is fully loaded, the code will execute.
-
 $(document).ready(function() {
 
     // Here I am caching some global variables that I will need
     var searchField = $('#q'),
-    searchValue = searchField.value;
-
-    // target the query and execute function
-    $('#q').keyup(function() {
+        searchValue = searchField.value;
 
     //.getJson is magic! I pass in the url of the data
-    $.getJSON('data/contacts.json', function (data) {
+    $.getJSON('data/contacts.json', function(data) {
+
+        console.log(data);
 
         //storing address book data in a local variable named addrBook, I think this may be my problem since its local. I need it to keep goin for my loop towards the bottom!
         var addrBook = data.addressBook,
 
-        //grabbing the count for the loop
-        count = addrBook.length;
-            
-        //clear any html in the target
-        $('#output').empty();
+            //grabbing the count for the loop
+            count = addrBook.length;
 
-        //checking the count
-        if (count > 0) {
+        $('#q').keyup(function() {
 
-            //special jquery loop. Assign each addr to obj
-            $.each(addrBook, function(i, obj) {
+            //clear any html in the target
+            $('#output').empty();
 
-               //check if there is a match using regEx
-                isItFound = obj.name.match(new RegExp(searchValue, "i"));
+            //checking the count
+            if (count > 0) {
 
-                if (isItFound) {
+                //special jquery loop. Assign each addr to obj
+                $.each(addrBook, function(i, obj) {
 
-                    //if its found, print out the option
-                    $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
+                    //check if there is a match using regEx
+                    isItFound = obj.name.match(new RegExp(searchValue, "i"));
+
+                    if (isItFound) {
+
+                        //if its found, print out the option
+                        $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
                     }
                 }); //close
-            } //close count
-        }); // end ajax call
-    }); //close keyup
-}); // dom ready close
+            } //close count 
+        }); //close keyup
+    }); // end getJson call
+}); // dom ready close​
