013c7a2e96f6050fd577ad54872634d1e9e51cae
Dave Levine <dmlevi@gmail.com>
2012-12-27 12:09:30 -0500

    moved keyup jquery call, still working on it

--- js/main.js
+++ js/main.js
@@ -1,27 +1,19 @@
-
-
-// document.ready waits for the entire dom to load. As soon as the dom is fully loaded, the code will execute.
 $(document).ready(function() {
 
-        var searchField = $('#q'),
+    var searchField = $('#q'),
         searchValue = searchField.value;
 
-    //.getJson is magic! I pass in the url of the data
     $.getJSON('data/contacts.json', function(data) {
 
-        //storing address book data in a local variable named addrBook, I think this may be my problem since its local. I need it to keep goin for my loop towards the bottom!
         var addrBook = data.addressBook,
-
-            //grabbing the count for the loop
             count = addrBook.length;
 
-            //clear any html in the target
-            $('#output').empty();
+        $('#output').empty();
 
-            //checking the count
+        $('#q').keyup(function() {
+
             if (count > 0) {
 
-                //special jquery loop. Assign each addr to obj
                 $.each(addrBook, function(i, obj) {
 
                     //check if there is a match using regEx
@@ -29,10 +21,10 @@ $(document).ready(function() {
 
                     if (isItFound) {
 
-                        //if its found, print out the option
                         $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
                     }
                 }); //close
             } //close count 
+        });
     }); // end getJson call
 }); // dom ready close​
