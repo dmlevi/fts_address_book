d5d68e25a820079d0c36160f5cdc25280c3879fb
Dave Levine <dmlevi@gmail.com>
2012-12-31 15:58:58 -0500

    started sprint 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+(function( $ ) {
+ 
+  $.fn.superBook = function(options) {
+
+    var settings =
+    {
+        'url'   :  '[]',
+        'output' : '[]'
+    };
+
+    return this.each(function() {
+  
+    $.getJSON(settings.url, function(data) {
+
+        var searchField = $(this),
+            addrBook = data.this,
+            count = addrBook.length;
+
+        $(this).keyup(function() {
+
+            var searchValue = searchField.val();
+
+            $(this).empty();
+
+            if (count > 0) {
+
+                $.each(addrBook, function(i, obj) {
+
+                    isItFound = obj.name.match(new RegExp(searchValue, "i"));
+
+                    if (isItFound) {
+
+                    $(this).append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
+                    }
+                 }); //close
+             } //close count 
+        }); //keyup close
+    }); // end getJson call
+    });
+
+  };
+})( jQuery );
+
+
 $(document).ready(function() {
 
     $.getJSON('data/contacts.json', function(data) {
