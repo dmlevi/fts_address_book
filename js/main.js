6f40da2e993dd29672a006db50b2a486b7840ed4
Dave Levine <dmlevi@gmail.com>
2013-01-01 15:48:53 -0500

    Update js/main.js

--- js/main.js
+++ js/main.js
@@ -1,84 +1,38 @@
-(function($){
- 
-    $.fn.extend({ 
-
-        ftsBook: function(options) {
-
-            var defaults = {
-                url: 'data/contacts.json',
-                searchField : '#q'
-            };
-                 
-            var options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-
-                $.getJSON(o.url, function(data) {
-
-                var searchField = (o.searchField),
-                    addrBook = data,
-                    count = addrBook.length;
-
-                $(o.searchField).keyup(function() {
-
-                var searchValue = searchField.val();
-
-                $(o.searchField).empty();
-
-                if (count > 0) {
-
-                $.each(addrBook, function(i, obj) {
-
-                    isItFound = obj.name.match(new RegExp(searchValue, "i"));
-
-                    if (isItFound) {
-
-                        $(o.searchField).append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
-                                }
-                            }); //close
-                        } //close count 
-                    }); //keyup close
-                }); // end getJson call
-             
-            });
-        }
-    });
-     
-})(jQuery);
-
-
-
-
-
 
 $(document).ready(function() {
 
-    $.getJSON('data/contacts.json', function(data) {
+    $(window).ftsBook({url: 'data/contacts.json', query : '#q', output : '#output'});
 
-        var searchField = $('#q'),
-            addrBook = data.addressBook,
-            count = addrBook.length;
+});
 
-        $('#q').keyup(function() {
 
-            var searchValue = searchField.val();
 
-            $('#output').empty();
+// $(document).ready(function() {
 
-            if (count > 0) {
+//     $.getJSON('data/contacts.json', function(data) {
 
-                $.each(addrBook, function(i, obj) {
+//         var searchField = $('#q'),
+//             addrBook = data.addressBook,
+//             count = addrBook.length;
 
-                    isItFound = obj.name.match(new RegExp(searchValue, "i"));
+//         $('#q').keyup(function() {
 
-                    if (isItFound) {
+//             var searchValue = searchField.val();
 
-                        $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
-                    }
-                }); //close
-            } //close count 
-        }); //keyup close
-    }); // end getJson call
-}); // dom ready close​
+//             $('#output').empty();
+
+//             if (count > 0) {
+
+//                 $.each(addrBook, function(i, obj) {
+
+//                     isItFound = obj.name.match(new RegExp(searchValue, "i"));
+
+//                     if (isItFound) {
+
+//                         $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
+//                     }
+//                 }); //close
+//             } //close count 
+//         }); //keyup close
+//     }); // end getJson call
+// }); // dom ready close​
