6c1f3ddf779ffc05d4cb3c01581e7d0f1872998b
Dave Levine <dmlevi@gmail.com>
2013-01-01 12:23:18 -0500

    Am I on the right page!? added original to bottom

--- js/main.js
+++ js/main.js
@@ -47,3 +47,38 @@
     });
      
 })(jQuery);
+
+
+
+
+
+
+$(document).ready(function() {
+
+    $.getJSON('data/contacts.json', function(data) {
+
+        var searchField = $('#q'),
+            addrBook = data.addressBook,
+            count = addrBook.length;
+
+        $('#q').keyup(function() {
+
+            var searchValue = searchField.val();
+
+            $('#output').empty();
+
+            if (count > 0) {
+
+                $.each(addrBook, function(i, obj) {
+
+                    isItFound = obj.name.match(new RegExp(searchValue, "i"));
+
+                    if (isItFound) {
+
+                        $('#output').append('<p><a href="mailto:' + obj.email + '">' + obj.name + '</a></p>');
+                    }
+                }); //close
+            } //close count 
+        }); //keyup close
+    }); // end getJson call
+}); // dom ready close​
