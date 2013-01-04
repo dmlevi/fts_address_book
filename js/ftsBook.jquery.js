3cb0048d74ee17dc59042330f624fb9cce5e9764
Dave Levine <dmlevi@gmail.com>
2013-01-04 12:45:28 -0500

    app removes names after delete

--- js/ftsBook.jquery.js
+++ js/ftsBook.jquery.js
@@ -23,7 +23,7 @@
 
               $(options.output).empty();
 
-              if (count > 0) {
+              if (count > 0 && searchValue !== "") {
 
                   $.each(addrBook, function(i, obj) {
 
