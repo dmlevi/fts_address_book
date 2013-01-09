08924a2bc135ff9e225974fff4238fb5e0353294
Apprentice 5 <apprentice5@steve-fts.local>
2013-01-09 17:43:05 -0500

    removed stupid plugin

--- js/ftsBook.jquery.js
+++ js/ftsBook.jquery.js
@@ -1,45 +1,41 @@
 (function( $ ){
 
-  $.fn.ftsBook = function( options ) {  
+  $.fn.ftsBook = function( options ) {
 
+    // Create some defaults, extending them with any options that were provided
     var settings = $.extend( {
-      url        : 'data/contacts.json',
+      url : 'data/contacts.json',
       query : '#q',
       output : '#output'
     }, options);
 
-    return this.each(function() {        
-        
+    return this.each(function() {
+
       $.getJSON(options.url, function(data) {
 
           var searchField = $(options.query),
-              imgs = $('img').attr(),
               addrBook = data.addressBook,
               count = addrBook.length;
 
           $('#q').keyup(function() {
 
-              var searchValue = searchField.val(),
-                    imgValue = imgs.val();
+              var searchValue = searchField.val();
 
               $(options.output).empty();
 
-              if (count > 0 && searchValue !== "") {
+              if (count > 0) {
 
                   $.each(addrBook, function(i, obj) {
 
                       isItFound = obj.name.match(new RegExp(searchValue, "i"));
-                      match = obj.id.match(new RegExp(imgValue, "i"));
                       template = $('#template').html();
 
                       if (isItFound) {
 
                           $(options.output).append(Mustache.to_html(template, obj));
-
-                          $(imgValue).addClass("transition");
                       }
                   }); //close
-              } //close count 
+              } //close count
           }); //keyup close
       }); // end getJson call
 
--- js/main.js
+++ js/main.js
@@ -1,39 +1,8 @@
 $(document).ready(function () {
 
-  
-
   $(window).ftsBook({
     url: 'data/contacts.json',
     query: '#q',
     output: '#output'
-  });
-
-  var $container = $('#am-container').hide(),
-    $imgs = $container.find('img'),
-    totalImgs = $imgs.length,
-    cnt = 0;
-  $imgs.each(function (i) {
-    var $img = $(this);
-    $('<img/>').load(function () {
-      ++cnt;
-      if (cnt === totalImgs) {
-        $imgs.show();
-        $container.montage({
-          liquid: 'true',
-          margin: '1',
-          minw: '140',
-          minh: '200',
-          maxh: '350',
-          alternateHeight: 'true',
-          alternateHeightRange: {
-            min: '200',
-            max: '400'
-          },
-          fixedHeight: 'null',
-          minsize: 'false',
-          fillLastRow: 'false'
-        });
-      }
-    }).attr('src', $img.attr('src'));
   });
 }); //ready
